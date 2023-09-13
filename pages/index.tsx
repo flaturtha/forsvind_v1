efa40c14040d5c4be71ee25097c27210912480c2
u404 <rich@nontechieguides.io>
2023-09-13 15:02:28 -0500

    px broken

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="relative flex flex-col mt-16 md:mt-64 lg:mt-16 md:mr-16 lg:mr-0 p-6 pl-8 md:ml-16 xl:ml-96 bg-white/75 md:bg-white/[0.35] lg:bg-white/50 lg:w-1/2">
         <p className="text-gray-800 text-3xl lg:text-3xl font-black md:text-5xl ">What is the most critical survival tool in&nbsp;your&nbsp;arsenal?</p>
         <p className="md:text-2xl text-center md:text-left mt-4 md:font-semibold text-gray-800 italic">&hellip; and are you using it properly?</p>
-        <button class="flex-1 px-6 py-2 mt-8 lg:w-64 self-center font-semibold select-none rounded-md text-gray-800 border-2 border-gray-800 hover:border-black hover:text-black">Find Out &#x2794;</button>
+        <button className="flex-1 px-6 py-2 mt-8 lg:w-64 self-center font-semibold select-none rounded-md text-gray-800 border-2 border-gray-800 hover:border-black hover:text-black">Find Out &#x2794;</button>
       </div>
 
       {/* EXTRA BUTTONS ETC WERE CUT FROM HERE! */}
